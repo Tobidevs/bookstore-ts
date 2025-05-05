6767b19582bc08c83f9be9605ac6dc45998563cf
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-05-05 16:36:03 -0500

    mongoose init

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,17 @@
 import express from "express";
-
+import { PORT, mongodbURL } from "./config.js"
+import mongoose from "mongoose"
 const app = express()
+
+app.get('/', (req, res, next) => {
+
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on port: ${PORT}`)
+})
+
+mongoose.connect(mongodbURL)
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'));
