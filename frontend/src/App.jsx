1ddaabf26b2ddd88efc0bd61a8804b145a473ba3
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-05-06 14:51:20 -0500

    Configured frontend routes + added pages
    Configured frontend routes with react routes dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import ShowBook from './pages/ShowBook'
+import EditBook from './pages/EditBook'
+import DeleteBook from './pages/DeleteBook'
+import CreateBook from './pages/CreateBook'
 
 const App = () => {
   return (
-    <div className='bg-red-500'>App</div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/details' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
+    </Routes>
   )
 }
 
